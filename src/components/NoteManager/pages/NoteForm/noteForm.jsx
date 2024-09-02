54e1ebe304c6a19d78827dd2a4f6d996139d63ae
Meer Afzal <do4best@gmail.com>
2024-09-02 18:57:46 +0500

    create form is being normalized

--- src/components/NoteManager/pages/NoteForm/noteForm.jsx
+++ src/components/NoteManager/pages/NoteForm/noteForm.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {PencilFill, TrashFill} from "react-bootstrap-icons";
+import s from './sytle.module.css';
 
-function NoteForm({title}) {
+function NoteForm({title,onClickEdit,onClickDelete,onSubmit}) {
+    const [formValue,setFormValue] = useState({title:"",content:""})
+    const updateFormValue=(e)=>{
+        const name = e.target.name;
+        const value = e.target.value;
+        setFormValue({...formValue,[name]:value})
+    }
     const actionIcons=(
         <>
 <div className="col-1">
-    <PencilFill/>
+    {onClickEdit && <PencilFill/>}
 </div>
             <div className="col-1">
-                <TrashFill/>
+                { onClickDelete &&  <TrashFill/>}
             </div>
 
         </>
@@ -16,34 +23,34 @@ function NoteForm({title}) {
     const titleInput=(
         <>
             <label htmlFor="" className="form-control">Title</label>
-            <input type="text" name={"title"} className={"form-control"}/>
+            <input onChange={""} type="text" name={"title"} className={"form-control"}/>
         </>
     )
     const contentInput=(
         <>
             <label htmlFor="" className="form-control">Content</label>
-            <textarea type="text" name={"content"} className={"form-control"} rows={"5"}/>
+            <textarea onChange={""} type="text" name={"content"} className={"form-control"} rows={"5"}/>
         </>
     )
     const submitBtn =(
         <div>
-            <div className="btn btn-primary">Submit</div>
+            <div className="btn btn-primary" onClick={()=>  updateFormValue}>Submit</div>
         </div>
     )
     return (
         <>
-            <div>
+            <div className={s.container}>
                 <div className={"row justify-content-around space-between"}>
                     <div className={"col-10"}>
-                        <h2 className="mb-3">{title}</h2>
+                        <h2 className={`mb-3 `}>{title}</h2>
                     </div>
 
                     {actionIcons}
                     <div></div>
                 </div>
-                <div className={"mb-3"}>{titleInput}</div>
+                <div className={`mb-3 ${s.title_input_container}`}>{titleInput}</div>
                 <div className={"mb-3"}>{contentInput}</div>
-                {submitBtn}
+                {onSubmit && {submitBtn}}
             </div>
         </>
     );
